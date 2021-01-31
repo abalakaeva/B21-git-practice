18f85f6d7bc88ec9cc27e86238773fd6a7287ed3
Anna Wood <aneli1986@yahoo.com>
2021-01-31 09:58:24 -0500

    more work on featureA on new branch

--- src/test/java/FeatureCode.java
+++ src/test/java/FeatureCode.java
@@ -2,6 +2,7 @@ public class FeatureCode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A had started");
+        System.out.println("b21 is learning more Git");
 
 
     }
