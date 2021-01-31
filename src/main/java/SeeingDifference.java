fd248a485e4ee064505ef33d8e00166e5fdc7ffe
Anna Wood <aneli1986@yahoo.com>
2021-01-31 14:44:58 -0500

    Added more work in Master branch SeeingDifference.java

--- src/main/java/SeeingDifference.java
+++ src/main/java/SeeingDifference.java
@@ -4,5 +4,6 @@ public class SeeingDifference {
     // this is new line of work in master that movie6 does not know about
     // this is new line of work in movie6
 
+    // I have more work to do
 
 }
