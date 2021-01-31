46d966694c47bcd266eeaeca4204f7662a010f21
Anna Wood <aneli1986@yahoo.com>
2021-01-31 12:32:37 -0500

    movie3 branch watching on the phone before going back to living room

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
         System.out.println("B21 future SDET learning Git");
         // movie on the phone 10 min
         // movie on the phone 20 min
+        // another final 10 minutes before going back to the living room
 
     }
 }
